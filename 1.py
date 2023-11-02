2e68ab4910dbedfb0be39db25aa4ae1707631af6
v7ja <142932805+v7ja@users.noreply.github.com>
2023-11-02 18:45:56 +0300

    Update 1.py

--- 1.py
+++ 1.py
@@ -146,7 +146,6 @@ async def main(app, msg):
                 wa = open('user.txt','r').read()
                 await tele.set_username(wa)
                 await tele.update_profile(first_name="aBooD YaBh #1")
-                await
 tele.update_profile(picture="https://telegra.ph/file/3c7f2248c13a305b4dd05.jpg")
                 me = await tele.get_me()
                 pho = me.phone_number
